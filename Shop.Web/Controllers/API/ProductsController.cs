75b515285b7e14e2b384ddb0eb00ba269964173e
Rocki Uriol <rocki.uriol@gmail.com>
2019-09-27 00:49:36 -0500

    API Products Controllers

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿namespace Shop.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Data;
+    [Route("api/[Controller]")]
+
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(productRepository.GetAll());
+        }
+
+    }
+}
